545340d1ad92769b62064d9ff30c408bb6f9f227
AnmAbdullah977 <anmabdullah072@gmail.com>
2025-05-31 22:44:49 +0600

    Day 10 & 11

--- RDSML-Day-11-Descriptives Statistics in R/scater.R
+++ RDSML-Day-11-Descriptives Statistics in R/scater.R
@@ -1,6 +1,6 @@
 # install.packages("ggplot2")
 library(ggplot2)
-install.packages("ggplot2")
+
 dataset = mtcars
 
 # Generating a basic scatter plot
@@ -14,7 +14,43 @@ ggplot(dataset, aes(x = wt, y = mpg)) +
 # Adding the regression line
 ggplot(dataset, aes(x = wt, y = mpg)) +
   geom_point(size=1, shape=18) +
-  geom_smooth(method = 1m)
+  geom_smooth(method = lm)
+
+# Removing the confidence interval
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE)
 
 # Loess method
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth()
 
+# Change the line type and color
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18) +
+  geom_smooth(method = lm, linetype = "dashed", 
+              color = "darkblue")
+
+# Changing the confidence interval color
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=18, color = "blue") +
+  geom_smooth(method = lm, linetype = "dashed", 
+              color = "darkred", fill = "blue")
+
+# Scatter plots with multiple groups
+
+# Converting the cyl column from a numeric to factor variable
+dataset$cyl = as.factor(dataset$cyl)
+
+# Changing the point shapes by the levels of cyl
+ggplot(dataset, aes(x = wt, y = mpg, shape = cyl)) +
+  geom_point()
+
+# Changing point shapes and colors
+ggplot(dataset, aes(x = wt, y = mpg, shape = cyl, color = cyl)) +
+  geom_point()
+
+# Changing point shapes, colors and sizes
+ggplot(dataset, aes(x = wt, y = mpg, shape = cyl, color = cyl, size = cyl)) +
+  geom_point()
